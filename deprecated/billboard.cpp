45529325ec171c0a935fc41f33264b9e05a79827
Avergreen <99834400+Avergreen16@users.noreply.github.com>
2026-08-17 17:53:17 -0400

    fixing some errors with git

--- deprecated/billboard.cpp
+++
@@ -1,8 +0,0 @@
-#include "billboard.hpp"
-
-void Billboard_animation::set_animation(uint32_t id) {
-    if(current_animation != id) {
-        current_animation = id;
-        current_time = 0.0f;
-    }
-}
